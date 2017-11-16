45b562ef59a4146adcf56d1b0ff4b3e81a266fe9
Leon <Leon.ID@QQ.COM>
2017-11-16 19:57:55 +0800

    首次提交

---
+++ WatchDog/MotionForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WatchDog
+{
+    partial class MotionForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LogLabel = new System.Windows.Forms.Label();
+            this.MotionPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MotionPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LogLabel
+            // 
+            this.LogLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.LogLabel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.LogLabel.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LogLabel.ForeColor = System.Drawing.Color.White;
+            this.LogLabel.Location = new System.Drawing.Point(242, 0);
+            this.LogLabel.Name = "LogLabel";
+            this.LogLabel.Padding = new System.Windows.Forms.Padding(5);
+            this.LogLabel.Size = new System.Drawing.Size(400, 411);
+            this.LogLabel.TabIndex = 0;
+            // 
+            // MotionPictureBox
+            // 
+            this.MotionPictureBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.MotionPictureBox.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.MotionPictureBox.Location = new System.Drawing.Point(0, 186);
+            this.MotionPictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.MotionPictureBox.MaximumSize = new System.Drawing.Size(400, 225);
+            this.MotionPictureBox.MinimumSize = new System.Drawing.Size(400, 225);
+            this.MotionPictureBox.Name = "MotionPictureBox";
+            this.MotionPictureBox.Size = new System.Drawing.Size(400, 225);
+            this.MotionPictureBox.TabIndex = 1;
+            this.MotionPictureBox.TabStop = false;
+            // 
+            // MotionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(642, 411);
+            this.Controls.Add(this.MotionPictureBox);
+            this.Controls.Add(this.LogLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MotionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "爸爸，吃饭！";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MotionForm_FormClosing);
+            this.Shown += new System.EventHandler(this.MotionForm_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.MotionPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LogLabel;
+        private System.Windows.Forms.PictureBox MotionPictureBox;
+    }
+}
+
---
+++ WatchDog/MotionForm.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Vision.Motion;
+using System.Diagnostics;
+using System.Threading;
+
+namespace WatchDog
+{
+    public partial class MotionForm : Form
+    {
+        /*
+         * DOC:
+         * http://www.aforgenet.com/framework/features/motion_detection_2.0.html
+         */
+         
+        //忽略警报事件，防止瞬间多次触发警报
+        bool IgnoreAlert = false;
+        //摄像头
+        FilterInfoCollection VideoDevicesList ;
+        IVideoSource VideoSource;
+        //运动监视
+        MotionDetector LunchDetector;
+
+        IMotionDetector motionDetector = new TwoFramesDifferenceDetector();//new SimpleBackgroundModelingDetector()
+        IMotionProcessing motionProcessing = new MotionAreaHighlighting();//new MotionAreaHighlighting()
+
+        public MotionForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MotionForm_Shown(object sender, EventArgs e)
+        {   
+            AppendLog("程序已加载...");
+            try
+            {
+                AppendLog("正在读取摄像头设备列表...");
+                //初始化摄像头设备
+                VideoDevicesList = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+                AppendLog("使用默认摄像头设备...");
+                //使用默认设备
+                VideoSource = new VideoCaptureDevice(VideoDevicesList[0].MonikerString);
+                //绑定事件
+                AppendLog("注册警报事件");
+                VideoSource.NewFrame += new NewFrameEventHandler(Alert);
+                AppendLog("正在启动摄像头...");
+                //启动摄像头
+                VideoSource.Start();
+            }
+            catch (Exception ex)
+            {
+                AppendLog("无法连接或启动摄像头，程序即将退出...\n{0}",ex.Message);
+                MessageBox.Show(string.Format("无法连接摄像头:\n{0}",ex.Message));
+                Application.Exit();
+            }
+            AppendLog("设备初始化完成！开始监视...");
+
+            //运动监视
+            LunchDetector = new MotionDetector(motionDetector,motionProcessing);
+            AppendLog("运行监视创建完成...");
+            AppendLog("————————————");
+        }
+
+        private void MotionForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                VideoSource.SignalToStop();//关闭摄像头
+                AppendLog("关闭摄像头...");
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                AppendLog("关闭摄像头遇到错误：\n{0}",ex.Message);
+            }
+        }
+
+        private void AppendLog(string LogMessage, params object[] LogValues)
+        {
+            AppendLog(string.Format(LogMessage,LogValues));
+        }
+
+        private void AppendLog(string LogMessage)
+        {
+            Debug.Print(string.Format("{0} : {1}", DateTime.Now.ToString(), LogMessage));
+
+            this.Invoke(new Action(delegate{
+                //LogLabel.Text += string.Format("{0} : {1}\n", DateTime.Now.ToString(), LogMessage);
+                LogLabel.Text = string.Format("{0} : {1}\n", DateTime.Now.ToString(), LogMessage) + LogLabel.Text;
+                LogLabel.Invalidate();
+            }));
+        }
+
+        private void Alert(object sender,NewFrameEventArgs e)
+        {
+            this.BackgroundImage = e.Frame.Clone() as Image;
+            float Result = LunchDetector.ProcessFrame(e.Frame.Clone() as Bitmap);
+            if ( Result > 0.0001)
+            {
+                if (IgnoreAlert) return;
+                IgnoreAlert = true;
+                AppendLog("发现移动对象 " + Result.ToString());
+
+                //TODO:触发监视警报
+                Debug.Print(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " : " + Result.ToString());
+                Speak();
+
+                //解除警报事件，防止瞬间重复触发
+                new Thread(new ThreadStart(delegate {
+                    Thread.Sleep(3000);
+                    IgnoreAlert = false;
+                })).Start();
+            }
+            MotionPictureBox.BackgroundImage = (motionDetector as TwoFramesDifferenceDetector).MotionFrame.ToManagedImage();
+            //GC.Collect();
+        }
+
+        private void Speak()
+        {
+            //using (SpeechSynthesizer Speaker = new SpeechSynthesizer())
+            SpeechSynthesizer Speaker = new SpeechSynthesizer();
+            Speaker.SpeakCompleted += new EventHandler<SpeakCompletedEventArgs>((s,e)=> { (s as SpeechSynthesizer).Dispose(); });
+            Speaker.SpeakAsync("爸爸，吃饭！");
+        }
+    }
+}
